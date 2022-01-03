16ceb91c75914c953242619cca8773c6273c0191
morvai577 <thevaimor@icloud.com>
2022-01-03 21:56:16 +1300

    Added new flexible gradientlayout for profile page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,18 @@
-export default () => <div>home</div>;
+import GradientLayout from "../components/gradientLayout";
+
+const Home = () => {
+  return (
+    <GradientLayout
+      roundImage
+      color="yellow"
+      subtitle="profile"
+      title="Vai Mor"
+      description="15 public playlists"
+      image="https://media-exp1.licdn.com/dms/image/C5603AQHUnPNMt3bitA/profile-displayphoto-shrink_800_800/0/1577415249606?e=1646870400&v=beta&t=cHalC5gBOnVBoUxuaKJsz0sCqsYOK24hp7J9knGWDRU"
+    >
+      <div>home page</div>
+    </GradientLayout>
+  );
+};
+
+export default Home;
